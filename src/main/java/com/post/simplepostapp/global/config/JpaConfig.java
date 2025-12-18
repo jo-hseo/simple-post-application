a075557c0fdb37d0a9c401f16fd5c62dc1d18fc1
jo-hseo <ellejo1110@gmail.com>
2025-12-18 11:01:48 +0900

    feat : auditing 설정 추가

---
+++ src/main/java/com/post/simplepostapp/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.post.simplepostapp.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
